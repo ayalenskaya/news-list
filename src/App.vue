fed2f642b00091e25d51de23abcfabdaa9a5674e
ayalenskaya <kamio-vk@yandex.ru>
2023-08-21 15:12:02 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <news-list/>
+</template>
+
+<script>
+import NewsList from '@/components/NewsList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NewsList
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,400;6..12,700&display=swap');
+
+* {
+  box-sizing: border-box;
+  outline: none;
+}
+
+html,
+body {
+  min-height: 100%;
+  margin: 0;
+  font-family: Nunito Sans;
+}
+</style>
